fb643c4fc24e4a8779c5c23c98284cdd1cf8a972
Ian Shaw <ianedwardshaw@gmail.com>
2018-04-24 21:09:07 -0700

    Set up proper index distribution and made portfolio BTC value function.

--- Packages/API/Bittrex/helper.py
+++ Packages/API/Bittrex/helper.py
@@ -54,17 +54,42 @@ def get_recent_data(bit2, coins = ['ETH', 'XRP', 'LTC', 'DASH', 'XMR'], freq = '
 		    X_df = X_df.join(x_df, how = 'inner')
 	return X_df
 
-def trade_on_weights(bit1, bit2, weights):
+def get_portfolio_worth_in_BTC(bit1):
+	'''
+	Function to calculate the BTC worth of the current portfolio on Bittrex.
+	:param bit1: Bittrex object v1
+	return: the portfolio worth in BTC	
+	'''
+	#Get portfolio balance by currency
+	balance_df = pd.DataFrame(bit1.get_balances()['result'])
+	#Get the last transacted price by Market
+	sum_df = pd.DataFrame(bit1.get_market_summaries()['result'])
+	sum_df['BaseCurrency'] = sum_df.MarketName.apply(lambda x: x.split('-')[0])
+	#Only transact in BTC base markets
+	sum_df = sum_df.loc[(sum_df.BaseCurrency == 'BTC')].copy()
+	sum_df['Currency'] = sum_df.MarketName.apply(lambda x: x.split('-')[-1])
+	#Merge the two tables
+	comb_df = balance_df.merge(sum_df,on='Currency')
+	#Get the Current BTC balance
+	BTC_val = balance_df.loc[balance_df.Currency == 'BTC','Balance'].values[0]
+	#Add the converted value of all other coins
+	BTC_val += comb_df.eval('Balance*Last').sum()
+	return BTC_val
+
+def trade_on_weights(bit1, bit2, weights,trade_basement_perc=1):
 	'''
 	This function changes the portfolio allocation to the specified proportional weights.
-	This uses limit orders at the most recetn recorded transaction price.
+	This uses limit orders at the most recent recorded transaction price.
 	Bittrex	does not support market orders.
 
 	:param bit1: bittrex object of v1.1
 	:param bit2: bittrex object of v2
 	:param weights: dictionary of desired portfolio allocation, keys are coin tickers, values are floats between 0 and 1
 	'''
-	trade_BTC_bal = (bit1.get_balance('BTC'))['result']['Available']
+	trade_BTC_bal = get_portfolio_worth_in_BTC(bit1) 
+	#only trade if trade casues trade_basement_perc% difference in holdings
+	#this is to avoind paying transaction fees on small changes
+	min_trade = 0.01*trade_basement_perc*trade_BTC_bal
 	# Loop through keys
 	for coin in weights:
 		market = 'BTC-{}'.format(coin)
@@ -76,24 +101,31 @@ def trade_on_weights(bit1, bit2, weights):
 		#Find then current going rate of the coin
 		last_rate = float(bit2.get_latest_candle(market=market, tick_interval='oneMin')['result'][0]['L'])
 		#cancel any open orders
-		open_orders = bit1.get_open_orders(market='BTC-{}'.format(coin))['result']
-		if open_orders:
-			for order in open_orders:
-				bit1.cancel(uuid=order['OrderUuid'])
+#		open_orders = bit1.get_open_orders(market='BTC-{}'.format(coin))['result']
+#		if open_orders:
+#			for order in open_orders:
+#				bit1.cancel(uuid=order['OrderUuid'])
 		#find balance of the currency in question
 		coin_avail = (bit1.get_balance(coin))['result']['Available']
 		if coin_avail is None:
 			coin_avail = 0
 		#find the difference between our amount and the desried amount
-		desried_amount = weights[coin] * trade_BTC_bal / last_rate
+		desired_amount = weights[coin] * trade_BTC_bal / last_rate
 		trade_amount = desired_amount - coin_avail
-		# if Sell
-		if trade_amount < 0.001:
-			print('Selling {} of {}'.format(abs(trade_amount), coin))
-			print(bit1.sell_limit(market=market, quantity=abs(trade_amount)
-				, rate=last_rate))
-		#if Buy
-		elif trade_amount > 0.001: 
-			print('Buying {} of {}'.format(trade_amount, coin))
-			print(bit1.buy_limit(market=market,
-				 quantity=trade_amount, rate=last_rate))
+		print('{}.Current Position: {:.2E}. Desired Position: {:.2E}.'.format(
+			coin,coin_avail,desired_amount))
+		trade_size_BTC = abs(trade_amount*last_rate)
+		if trade_size_BTC > min_trade: 
+			# if Sell
+			if trade_amount < -0.0001:
+				print('Selling {} of {}'.format(abs(trade_amount), coin))
+#				print(bit1.sell_limit(market=market, quantity=abs(trade_amount)
+#					, rate=last_rate))
+			#if Buy
+			elif trade_amount > 0.001: 
+				print('Buying {} of {}'.format(trade_amount, coin))
+#				print(bit1.buy_limit(market=market,
+#					 quantity=trade_amount, rate=last_rate))
+		elif abs(trade_amount) > 0:
+			print('Not trading. {:2E} BTC worth of {} is below {}% of portfolio[{:2E} BTC].'.format(
+		trade_size_BTC,coin,trade_basement_perc,min_trade))
--- Research/Index/index_rebalance.py
+++ Research/Index/index_rebalance.py
@@ -1,29 +1,36 @@
-
 __author__ = 'Ian'
 
 from Packages.API.Bittrex import helper
 import pandas as pd
 from coinmarketcap import Market
 
-def get_index_weights():
+def get_index_weights(bit1):
 	'''
+	param bit1: bittrex object of v1.1
 	return: Dictionary of Coin:market_cap proportion
 	'''
-
 	#instantiate coinmarketcap object
 	cmc = Market()
 	market_df = pd.DataFrame(cmc.ticker())
-
+	#Keep only coins traded on bittrex
+	market_df = market_df.loc[market_df['symbol'].isin(
+		pd.DataFrame(bit1.get_currencies()['result']).Currency.unique())].copy()
 	#now weight based on market cap
 	market_df['market_cap_usd'] = market_df['market_cap_usd'].astype(float)
 	market_df['index_weight'] = market_df['market_cap_usd']/market_df['market_cap_usd'].sum()
-
-	#for now, just drop the BTC enty
+	#find minimum Bittrex trade size, as function of
+	#portfolio value. Currently 0.001 BTC
+	min_perc = 0.001/(helper.get_portfolio_worth_in_BTC(bit1))
+	#filter to only trade if above percentage threshold
+	market_df['index_weight'] = market_df['index_weight'].where(
+		market_df['index_weight'] >min_perc,0)
+	#rebalance to reflect current weight distribution
+	market_df['index_weight'] = market_df['index_weight']/market_df['index_weight'].sum()
+	market_df['index_weight'].fillna(0,inplace=True)
+	#Since Base currency is BTC, do not trade 
 	market_df = market_df.loc[market_df['symbol'] != 'BTC'].copy()
-
 	#create dictionary of market and portfolio weights
 	weight_dict = dict(zip(market_df.symbol,market_df.index_weight))
-
 	return weight_dict
 
 def run_index():
@@ -32,9 +39,9 @@ def run_index():
 	to those weights.
 	'''
 	key_json_path = '/Users/ianshaw/.goods/exchange_keys.json'
-	key_name = 'bittrex'
+	key_name = 'bittrex_ro'
 	bit1, bit2 = helper.instantiate_bittrex_objects(key_name,key_json_path)
-	weights = get_index_weights()
+	weights = get_index_weights(bit1)
 	helper.trade_on_weights(bit1, bit2,weights)
 
 print('Rebalancing Index Fund')
