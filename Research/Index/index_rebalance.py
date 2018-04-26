032523767d28a9e6fb7ba34f0045596dfa960460
Ian Shaw <ianedwardshaw@gmail.com>
2018-04-25 21:04:59 -0700

    Update index rebalance in Execution

--- Research/Index/index_rebalance.py
+++ Research/Index/index_rebalance.py
@@ -19,8 +19,10 @@ def get_index_weights(bit1):
 	market_df['market_cap_usd'] = market_df['market_cap_usd'].astype(float)
 	market_df['index_weight'] = market_df['market_cap_usd']/market_df['market_cap_usd'].sum()
 	#find minimum Bittrex trade size, as function of
-	#portfolio value. Currently 0.001 BTC
-	min_perc = 0.001/(helper.get_portfolio_worth_in_BTC(bit1))
+	#portfolio value. Currently 0.001 BTC.
+	#Only trade 2x the minimum
+	#That way we can get out if moves against us
+	min_perc = 2 * 0.001/(helper.get_portfolio_worth_in_BTC(bit1))
 	#filter to only trade if above percentage threshold
 	market_df['index_weight'] = market_df['index_weight'].where(
 		market_df['index_weight'] >min_perc,0)
